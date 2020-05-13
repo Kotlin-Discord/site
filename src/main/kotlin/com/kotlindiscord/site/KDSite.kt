bf4882cbc13192c87ef105e67ccd5b371aaa9add
Gareth Coles <gdude2002@gmail.com>
2020-05-13 16:28:11 +0100

    Disable "undocumented" linting for now

--- src/main/kotlin/com/kotlindiscord/site/KDSite.kt
+++ src/main/kotlin/com/kotlindiscord/site/KDSite.kt
@@ -1,7 +1,7 @@
 package com.kotlindiscord.site
 
 // Sometimes you just want to commit the layout.
-@Suppress("EmptyClassBlock", "UndocumentedPublicClass")
+@Suppress("EmptyClassBlock")
 class KDSite {
 
 }
