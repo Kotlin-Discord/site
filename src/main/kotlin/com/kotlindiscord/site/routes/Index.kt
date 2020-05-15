575a8d43148da056d89a057737db33a9a7ec33c9
Gareth Coles <gdude2002@gmail.com>
2020-05-15 15:31:13 +0100

    Remove unused function

--- src/main/kotlin/com/kotlindiscord/site/routes/Index.kt
+++ src/main/kotlin/com/kotlindiscord/site/routes/Index.kt
@@ -1,18 +1,9 @@
 package com.kotlindiscord.site.routes
 
 import com.kotlindiscord.site.components.route
-import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.pebble.PebbleContent
 import io.ktor.response.respond
-import io.ktor.routing.get
-import io.ktor.routing.routing
-
-fun register(app: Application) {
-    app.routing {
-        get("/", indexGet)
-    }
-}
 
 val indexGet = route {
     call.respond(PebbleContent("index.html.peb", mapOf("who" to "world")))
