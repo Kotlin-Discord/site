449905b94c3f851f02e7af0cd1c6f9307f2aa3dd
Gareth Coles <gdude2002@gmail.com>
2020-05-16 16:10:33 +0100

    Collapse sidebar on larger resize

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,3 +3,9 @@ const mobileMenu = $(".mobile-menu").first()
 mobileMenu.sidebar({
     exclusive: true,
 }).sidebar("attach events", ".mobile-menu-button")
+
+$(window).resize(function() {
+    if ($(window).width() > 992) {
+        mobileMenu.sidebar("hide")
+    }
+})
