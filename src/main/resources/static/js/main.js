b3c8883b8c810dbc99d7c8c2110fd7f02f23e928
Gareth Coles <gdude2002@gmail.com>
2020-05-18 15:24:20 +0100

    Rules/policies page

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,5 @@
+// Set up sticky desktop sidebar, and collapsed mobile sidebar
+
 const desktopMenu = $("#desktop-menu").first()
 const mobileMenu = $("#mobile-menu").first()
 
@@ -9,8 +11,15 @@ mobileMenu.sidebar({
     exclusive: true,
 }).sidebar("attach events", ".mobile-menu-button")
 
-$(window).resize(function() {
+$(window).resize(function () {
     if ($(window).width() > 992) {
         mobileMenu.sidebar("hide")
     }
 })
+
+// If we have accordions, set them up
+
+$('.ui.accordion')
+    .accordion({
+        exclusive: false,
+    })
