20bdd1afc19bad5b6318980124cf406376390b58
Gareth Coles <gdude2002@gmail.com>
2020-05-28 14:25:30 +0100

    Sentry, logback and build info

--- src/main/kotlin/com/kotlindiscord/site/KDSite.kt
+++ src/main/kotlin/com/kotlindiscord/site/KDSite.kt
@@ -3,9 +3,20 @@ package com.kotlindiscord.site
 import com.kotlindiscord.site.components.*
 import io.ktor.application.Application
 import io.ktor.locations.KtorExperimentalLocationsAPI
+import io.sentry.Sentry
+import mu.KotlinLogging
 
 @KtorExperimentalLocationsAPI
 fun Application.main() {
+    val logger = KotlinLogging.logger {}
+
+    if (System.getenv().getOrDefault("SENTRY_DSN", null) != null) {
+        val sentry = Sentry.init()
+        sentry.release = buildInfo.version
+    }
+
+    logger.info { "Starting KDSite version ${buildInfo.version}." }
+
     // Install Locations first since other components use it
     installLocations(this)
 
