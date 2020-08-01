d2caf19de1b51c416ca08aedbf34c645cff8377c
Gareth Coles <gdude2002@gmail.com>
2020-08-01 12:52:09 +0100

    Force a rebuild

--- src/main/kotlin/com/kotlindiscord/site/routes/api/Index.kt
+++ src/main/kotlin/com/kotlindiscord/site/routes/api/Index.kt
@@ -3,5 +3,8 @@ package com.kotlindiscord.site.routes.api
 import com.kotlindiscord.site.components.apiRoute
 
 val apiIndex = apiRoute {
-    mapOf("message" to "Hello there, traveller! Please contact us directly if you'd like to use our API.")
+    mapOf(
+        "message" to
+                "Hello there, traveller! Please contact us directly if you'd like to use our API."
+    )
 }
