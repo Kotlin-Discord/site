163ef103c023e031cc50a9ffd5dc610211af650d
Gareth Coles <gdude2002@gmail.com>
2020-08-25 21:50:48 +0100

    Don't rely on exceptions for model checks, use nulls

--- src/main/kotlin/com/kotlindiscord/site/routes/api/Users.kt
+++ src/main/kotlin/com/kotlindiscord/site/routes/api/Users.kt
@@ -12,7 +12,6 @@ import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.util.*
 import mu.KotlinLogging
-import org.jetbrains.exposed.dao.exceptions.EntityNotFoundException
 import org.jetbrains.exposed.sql.SizedCollection
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 
@@ -39,18 +38,24 @@ val apiUsersPost = apiRoute {
     val model = call.receive<UserModel>()
 
     newSuspendedTransaction {
-        try {
-            val user = User[model.id]
+        var user = User.getOrNull(model.id)
 
-            user.userName = model.username
-            user.discriminator = model.discriminator
-            user.avatarUrl = model.avatarUrl
-            user.present = model.present
+        if (user == null) {  // User doesn't exist, so we need to create it
+            logger.debug { "Entity not found: ${model.id}" }
 
-            val givenRoles = user.roles.map { it.id.value }.toSet()
-            val roles = user.roles.filter { model.roles.contains(it.id.value) }.toMutableList()
+            User.new(model.id) {
+                userName = model.username
+                discriminator = model.discriminator
+                avatarUrl = model.avatarUrl
+                present = model.present
+            }
 
-            for (roleId in model.roles - givenRoles) {
+            commit()  // Exposed has a stupid bug that requires us to add roles after user creation is committed
+
+            user = User[model.id]
+            val roles = mutableListOf<Role>()
+
+            for (roleId in model.roles) {
                 val role = Role.getOrNull(roleId) ?: return@newSuspendedTransaction call.respond(
                     HttpStatusCode.NotFound,
                     mapOf("error" to "Unknown role ID: $roleId")
@@ -62,22 +67,16 @@ val apiUsersPost = apiRoute {
             user.roles = SizedCollection(roles)
 
             null  // Return nothing if we're successful
-        } catch (e: EntityNotFoundException) {
-            logger.debug(e) { "Entity not found: ${model.id}" }
+        } else {  // User exists, so we should update it
+            user.userName = model.username
+            user.discriminator = model.discriminator
+            user.avatarUrl = model.avatarUrl
+            user.present = model.present
 
-            User.new(model.id) {
-                userName = model.username
-                discriminator = model.discriminator
-                avatarUrl = model.avatarUrl
-                present = model.present
-            }
+            val givenRoles = user.roles.map { it.id.value }.toSet()
+            val roles = user.roles.filter { model.roles.contains(it.id.value) }.toMutableList()
 
-            commit()  // Exposed has a stupid bug that requires us to add roles after user creation is committed
-
-            val user = User[model.id]
-            val roles = mutableListOf<Role>()
-
-            for (roleId in model.roles) {
+            for (roleId in model.roles - givenRoles) {
                 val role = Role.getOrNull(roleId) ?: return@newSuspendedTransaction call.respond(
                     HttpStatusCode.NotFound,
                     mapOf("error" to "Unknown role ID: $roleId")
